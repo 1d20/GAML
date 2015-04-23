590db9c03d20e507f26e603bec276ffd896969ec
detonavomek <detonavomek@gmail.com>
2015-04-23 08:36:41 +0300

    add test scrapy application

---
+++ music_scrap/music_scrap/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for music_scrap project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'music_scrap'
+
+SPIDER_MODULES = ['music_scrap.spiders']
+NEWSPIDER_MODULE = 'music_scrap.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'music_scrap (+http://www.yourdomain.com)'
