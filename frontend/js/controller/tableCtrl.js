0b28769174c8649d4b4c55710d99888722880cc9
Alex <nycheporuk@gmail.com>
2015-04-28 06:44:10 +0300

    fill table with correct data

--- frontend/js/controller/tableCtrl.js
+++ frontend/js/controller/tableCtrl.js
@@ -1,77 +1,20 @@
 app.controller('tableCtrl', ['$scope', 'ngTableParams', '$http',
     function($scope, ngTableParams, $http) {
-        var data = [{
-            name: "Moroni",
-            age: 50
-        }, {
-            name: "Tiancum",
-            age: 43
-        }, {
-            name: "Jacob",
-            age: 27
-        }, {
-            name: "Nephi",
-            age: 29
-        }, {
-            name: "Enos",
-            age: 34
-        }, {
-            name: "Tiancum",
-            age: 43
-        }, {
-            name: "Jacob",
-            age: 27
-        }, {
-            name: "Nephi",
-            age: 29
-        }, {
-            name: "Enos",
-            age: 34
-        }, {
-            name: "Tiancum",
-            age: 43
-        }, {
-            name: "Jacob",
-            age: 27
-        }, {
-            name: "Nephi",
-            age: 29
-        }, {
-            name: "Enos",
-            age: 34
-        }, {
-            name: "Tiancum",
-            age: 43
-        }, {
-            name: "Jacob",
-            age: 27
-        }, {
-            name: "Nephi",
-            age: 29
-        }, {
-            name: "Enos",
-            age: 34
-        }];
-
-
-
-
         $scope.getSingers = function() {
             $http.get('/api/singers/').
             success(function(responce) {
                 console.log(responce);
-                //$scope.singers = responce;
+                //---add to table singers from server
                 $scope.tableParams = new ngTableParams({
                     page: 1, // show first page
                     count: 10 // count per page
                 }, {
                     total: responce.count, // length of data
                     getData: function($defer, params) {
-                        $defer.resolve(responce.result.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                        $defer.resolve(responce.results.slice((params.page() - 1) * params.count(), params.page() * params.count()));
                     }
                 });
                 console.log($scope.tableParams)
-                console.log($scope.singers)
             }).
             error(function(responce) {
                 console.log('error:', responce)
